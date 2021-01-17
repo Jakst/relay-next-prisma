5f44a1355cb71871a383ce8cae9394c924faeca3
Jakob Norlin <jakob.stahl91@gmail.com>
2021-01-17 18:59:06 +0100

    Use typescript

--- components/BlogPostPreview.tsx
+++ components/BlogPostPreview.tsx
@@ -1,14 +1,14 @@
 import { createFragmentContainer, graphql } from 'react-relay'
 
 const BlogPostPreview = ({ post }) => {
-  return <div key={post.id}>{post.title}</div>
+	return <div key={post.id}>{post.title}</div>
 }
 
 export default createFragmentContainer(BlogPostPreview, {
-  post: graphql`
-    fragment BlogPostPreview_post on BlogPost {
-      id
-      title
-    }
-  `,
+	post: graphql`
+		fragment BlogPostPreview_post on BlogPost {
+			id
+			title
+		}
+	`,
 })
---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { RelayEnvironmentProvider } from 'relay-hooks'
+import { createEnvironment } from '../lib/createEnvironment'
+
+export default function App({ Component, pageProps }) {
+	return (
+		<RelayEnvironmentProvider
+			environment={createEnvironment(pageProps.relayData)}
+		>
+			<Component {...pageProps} />
+		</RelayEnvironmentProvider>
+	)
+}
